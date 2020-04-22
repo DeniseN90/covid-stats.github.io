56a3e4f7c98d0ec239d66ac9e6f2ef1d8715e29a
Denise Nanni <denise_n@hotmail.it>
2020-04-22 22:23:57 +0200

    home component modified

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,26 +1,56 @@
-import { Component } from '@angular/core';
-import { HomeService } from './home.service';
+import { Component, OnInit, SimpleChanges } from '@angular/core';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'home-component',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-  countries: string[];
+export class HomeComponent implements OnInit {
+  
+
+  totalCases: number;
+  totalNewCases: number;
+  totalActiveCases: number;
+  totalCriticalCases: number;
+  totalRecoveredCases: number;
+  date: Date;
 
   error: any;
 
-  constructor(private homeService: HomeService) {
-    this.homeService.getAllCountries().subscribe(
+  constructor(private appService: AppService) {}
+
+  ngOnInit() {
+    this.totalCases = 0;
+    this.totalActiveCases = 0;
+    this.totalCriticalCases = 0;
+    this.totalRecoveredCases = 0;
+    this.totalNewCases = 0;
+    this.getWorldStats();
+  }
+
+  private getWorldStats() {
+    this.appService.getWorldStats().subscribe(
       (data) => {
-        console.log(data);
-        this.countries = data.response;
-        console.log(this.countries);
+        this.date = data.response[0].day;
+        this.getTotalCases(data.response);
       },
       (data) => {
         this.error = data.errors;
       }
     );
+  }
+
+  private getTotalCases(data: any[]) {
+    data.forEach((el) => {
+      if (el.country === 'All') {
+        this.totalCases += el.cases.total;
+        this.totalActiveCases += el.cases.active;
+        this.totalCriticalCases += el.cases.critical;
+        this.totalRecoveredCases += el.cases.recovered;
+        let s: string = el.cases.new.substring(1);
+        this.totalNewCases += Number(s);
+      }
+    });
   }
 }
