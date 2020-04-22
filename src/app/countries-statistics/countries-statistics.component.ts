77d6a6cc8385b1731be413aa19fff8ef6c2df221
Denise Nanni <denise_n@hotmail.it>
2020-04-22 22:25:38 +0200

    modified country statistic

--- src/app/countries-statistics/countries-statistics.component.ts
+++ src/app/countries-statistics/countries-statistics.component.ts
@@ -1,27 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-import { CountriesStatisticsService } from './countries-statistics.service';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-countries-statistics',
   templateUrl: './countries-statistics.component.html',
-  styleUrls: ['./countries-statistics.component.css']
+  styleUrls: ['./countries-statistics.component.css'],
 })
 export class CountriesStatisticsComponent implements OnInit {
   error: string;
 
-  constructor( private countriesStatsSerice: CountriesStatisticsService) {
-    this.countriesStatsSerice.getCountriesStats().subscribe(
+  countries: string[];
+
+  constructor(private appService: AppService) {
+    this.appService.getAllCountries().subscribe(
+      (data) => {
+       // console.log(data);
+        this.countries = data.response;
+      },
+      (data) => {this.error = data.errors;}
+    );
+  }
+
+  ngOnInit(): void {}
+
+ private getCountryHistory(country: string) {
+    this.appService.getHistory(country).subscribe(
       (data) => {
         console.log(data);
       },
       (data) => {
         this.error = data.errors;
-
       }
-    )
-   }
-
-  ngOnInit(): void {
+    );
   }
 
+  selectCountry(event){
+    // console.log(event.value);
+    this.getCountryHistory(event.value);
+
+  }
 }
