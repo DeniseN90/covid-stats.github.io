7d07b344c9d8c201977c17d25b1d1a9895eae08c
Denise Nanni <denise_n@hotmail.it>
2020-04-24 17:48:27 +0200

    imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatTableModule} from '@angular/material/table';
+import { MatTableModule } from '@angular/material/table';
 import { WorldStatisticsComponent } from './world-statistics/world-statistics.component';
 import { CountriesStatisticsComponent } from './countries-statistics/countries-statistics.component';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -17,6 +17,7 @@ import { FooterComponent } from './footer/footer.component';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { RenderLongNumbersPipe } from './shared/utils/render-long-numbers.pipe';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,8 @@ import { RenderLongNumbersPipe } from './shared/utils/render-long-numbers.pipe';
     MatSortModule,
     MatSelectModule,
     MatExpansionModule,
-    MatProgressSpinnerModule
+    MatProgressSpinnerModule,
+    ChartsModule
   ],
   exports: [HomeComponent, HeaderComponent],
   providers: [ RenderLongNumbersPipe ],
