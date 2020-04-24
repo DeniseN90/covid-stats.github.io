f610739d23d20fd50ffdc42b3dedd1ec34c9b1ac
Denise Nanni <denise_n@hotmail.it>
2020-04-24 10:21:28 +0200

    pipe added

---
+++ src/app/shared/utils/render-long-numbers.pipe.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'renderLongNumbers', pure: false })
+export class RenderLongNumbersPipe implements PipeTransform {
+  transform(value: any): string {
+    if (value === null) {
+      return;
+    }
+    if (value[0] === '+') {
+      value = value.substring(1);
+    }
+    let str = value.toString(10);
+    let newStr = '';
+    let remainder = str.length % 3;
+    let i = remainder;
+    if (remainder !== 0) {
+      newStr = newStr + str.substring(0, remainder) + "'";
+    }
+    for (i; i < str.length; i += 3) {
+      newStr += str.substring(i, i + 3);
+      if (str.length - i <= 3) {
+        continue;
+      }
+      newStr += "'";
+    }
+    return newStr;
+  }
+}
