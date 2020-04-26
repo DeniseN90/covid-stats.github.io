47bea9632d13bccc381db4b840cdf67f6e8023b1
Denise Nanni <denise_n@hotmail.it>
2020-04-26 09:56:11 +0200

    switch removed from ui

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ThemePalette } from '@angular/material/core';
 
+
 @Component({
   selector: 'header-component',
   templateUrl: './header.component.html',
