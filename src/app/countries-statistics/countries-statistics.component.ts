4d65f7e57caca47e97bf394c94c28f56e495e120
Denise Nanni <denise_n@hotmail.it>
2020-04-27 11:02:18 +0200

    fix filter

--- src/app/countries-statistics/countries-statistics.component.ts
+++ src/app/countries-statistics/countries-statistics.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { AppService } from '../app.service';
+import { MatSelect } from '@angular/material/select';
+import { take, takeUntil } from 'rxjs/operators';
+import { Subject, ReplaySubject } from 'rxjs';
 
 @Component({
   selector: 'app-countries-statistics',
@@ -10,26 +13,39 @@ export class CountriesStatisticsComponent implements OnInit {
   error: string;
 
   countries: string[];
+  
   selectedCountry: string;
   inputData: any[];
   loaded: boolean;
   mobile: boolean;
+  filter: any = '';
+
+  @ViewChild('countriesSelect') singleSelect: MatSelect; 
+  private _onDestroy = new Subject<void>();
+  public filteredCountries: ReplaySubject<string[]> = new ReplaySubject<string[]>(1);
 
   constructor(private appService: AppService) {
     this.loaded = true;
   }
 
   ngOnInit(): void {
+    this.filter = '';
     this.getAllCountries();
    // console.log(window.screen.width < 560);
     this.mobile = (window.screen.width < 560);
   //  console.log('mobile', this.mobile);
+  // this.filter.valueChanges
+  //     .pipe(takeUntil(this._onDestroy))
+  //     .subscribe(() => {
+  //       this.filterCountries();
+  //     });
   }
 
   private getAllCountries() {
     this.appService.getAllCountries().subscribe(
       (data) => {
         this.countries = data.response;
+        this.filteredCountries.next(this.countries.slice());
       },
       (data) => {
         this.error = data.errors;
@@ -54,6 +70,28 @@ export class CountriesStatisticsComponent implements OnInit {
       (data) => {
         this.error = data.errors;
       }
+    );
+  }
+
+  getFilter(event){
+    console.log(event);
+  }
+
+  private filterCountries() {
+    if (!this.countries) {
+      return;
+    }
+    // get the search keyword
+    let search = this.filter;
+    if (!search) {
+      this.filteredCountries.next(this.countries.slice());
+      return;
+    } else {
+      search = search.toLowerCase();
+    }
+    // filter the banks
+    this.filteredCountries.next(
+      this.countries.filter(country => country.toLowerCase().indexOf(search) > -1)
     );
   }
 
