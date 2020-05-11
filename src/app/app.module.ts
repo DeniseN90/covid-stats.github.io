1e33906da872338e768449d751122fe3f8afa2ae
Denise Nanni <denise_n@hotmail.it>
2020-05-11 08:13:17 +0200

    404 on reload fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import { MatInputModule } from '@angular/material/input';
 import { BarChartMobileComponent } from './countries-statistics/charts/mobile-charts/bar-chart-mobile.component';
 import { NgxGoogleAnalyticsModule, NgxGoogleAnalyticsRouterModule } from 'ngx-google-analytics';
 import { RateChartComponent } from './countries-statistics/charts/rate-chart/ratechart.component';
-
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -65,7 +65,7 @@ import { RateChartComponent } from './countries-statistics/charts/rate-chart/rat
     NgxGoogleAnalyticsRouterModule
   ],
   exports: [RouterModule],
-  providers: [RenderLongNumbersPipe],
+  providers: [RenderLongNumbersPipe, {provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
